05a988469fa5abbbf95744e93e6b719ac33cdd80
patel3mj@mail.uc.edu <patel3mj@mail.uc.edu>
2022-02-18 17:45:23 -0500

    got data in

--- data_collection/test.py
+++ data_collection/test.py
@@ -2,7 +2,8 @@
 
 from uldaq import (get_daq_device_inventory, DaqDevice, InterfaceType,
                    AiInputMode, Range, AInFlag)
-
+import csv
+file_header = ["c0","c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15"]
 try:
     # Get a list of available DAQ devices
     devices = get_daq_device_inventory(InterfaceType.USB)
@@ -14,14 +15,25 @@ try:
     ai_info = ai_device.get_info()
 
     # Read and display voltage values for all analog input channels
+    overall = []
     while True:
-        x = input('Anything')
-        if x:
+        x = input()
+        if x != "":
             break
+        
+        info_row= []
         for channel in range(ai_info.get_num_chans()):
             data = ai_device.a_in(channel, AiInputMode.SINGLE_ENDED,
                               Range.BIP10VOLTS, AInFlag.DEFAULT)
+            info_row.append(data)
             print('Channel', channel, 'Data:', data)
+        overall.append(info_row)
+        
+
+    with open("./dataset.csv",'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(file_header)
+        writer.writerows(overall)
 
     daq_device.disconnect()
     daq_device.release()
