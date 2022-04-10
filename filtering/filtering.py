5de5b83dce91d18b8ef7a2eee5d24ec3ebb774ff
patelmj <patelmj@github.com>
2022-04-10 17:50:18 -0400

    labelling plots

--- filtering/filtering.py
+++ filtering/filtering.py
@@ -21,21 +21,26 @@ def plot():
     data = pd.read_csv("../data/actual_single_foam.csv")
     #data = pd.read_csv("../data/just_motor2.csv")
     #To Cut out 2 thirds of the data to see the middle portion, do the following
-    data = data[1240:2480]
-    #data = data[0:1291]
+    #data = data[1240:2480]
+    data = data[2000:2480]
 
     raw_data = data[['tz']]
     raw_data = np.array(raw_data)
     time= np.array(data[["time(ms)"]])
     print(time[-1])
     figure,axis =plt.subplots(3,1)
-    #The following is for plotting the x and y label
-    figure.add_subplot(111,frame_on=False)
-    plt.tick_params(labelcolor="none",bottom=False,left=False)
-    plt.xlabel("time (ms)")
-    plt.ylabel("Torque (Nmm)")
-    axis[0].plot(time,raw_data,label = "Raw")
+    #The following is for plotting the x and y label for the whole plot
+    #figure.add_subplot(111,frame_on=False)
+    #plt.tick_params(labelcolor="none",bottom=False,left=False)
+    #plt.xlabel("time (ms)")
+    #plt.ylabel("Torque (Nmm)")
+    axis[0].plot(time,raw_data,label = "Torque Lift Axis")
+    axis[0].set(xlabel="time (ms)",ylabel='Torque (Nmm)')
     axis[0].legend()
+    axis[0].set(xlabel="time (ms)",ylabel='Torque (Nmm)')
+    axis[1].set(xlabel="time (ms)",ylabel='Torque (Nmm)')
+    axis[2].set(xlabel="time (ms)",ylabel='Torque (Nmm)')
+
 
     filtered_sig=bandPass(raw_data)
     axis[1].plot(time,filtered_sig,label="Filtered")
@@ -69,7 +74,7 @@ def plot():
 def bandPass(signal):
     fs      = 100.0 #This is the sample frequency that we are using in our data, 10 ms
     lowcut  = 1.0
-    highcut = 5.0
+    highcut = 10.0
     nyq     = 0.5*fs
     low     = lowcut/nyq
     high    = highcut/nyq
